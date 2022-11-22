6304b23016ba405f6b264b6b310d6c2bdff66c91
程述 <cs123456>
2022-11-22 16:37:19 +0800

    注册到Eureka注册中心中

--- src/main/java/com/ItemApp.java
+++ src/main/java/com/ItemApp.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @date 2022/11/18 11:19
  */
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = {"com.cs.item.controller","com.cs.item.service"})
 public class ItemApp {
 
